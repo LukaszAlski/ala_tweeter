d53dfdc462ade4d33317d9cc3db44ddcdaee8cbf
LukaszAlski <lukasz.alski@gmail.com>
2020-07-11 16:04:57 +0200

    AT-1. HibernateUtil, AppUser, implemented. Database configuration added.

---
+++ src/main/java/dao_dataAccessObject/UserDao.java
@@ -0,0 +1,19 @@
+package dao_dataAccessObject;
+
+import model.AppUser;
+
+import java.util.List;
+
+public interface UserDao {
+    List<AppUser> getAll();
+    void saveUser(AppUser user);
+    void deleteUser(long id);
+    AppUser getUserByEmail(String email);
+    AppUser getUserByLogin(String login);
+    List<AppUser> getUsersByName(String name);
+    boolean isUserValid(String login, String password);
+    List<AppUser> getFollowedUser(String login);
+    List<AppUser> getFollowers(String login);
+    void follow(String currentUserLogin, String userLoginToFollow);
+    void stopFollowing(String currentUserLogin, String userLoginTOStopFollow);
+}
---
+++ src/main/java/hibernate/util/HibernateUtil.java
@@ -0,0 +1,59 @@
+package hibernate.util;
+
+import net.bytebuddy.implementation.bytecode.member.HandleInvocation;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+
+public class HibernateUtil {
+    // tworzenie sinlegona zaczynamy od zrobienia instancji HibernateUtil, która będzie dostępna
+    // w kontekście całej aplikacji
+    private static HibernateUtil instance;
+    private final EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory("myDatabase");
+    private final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    public static HibernateUtil getInstance() {
+        if (null == instance) {
+            instance = new HibernateUtil();
+        }
+        return instance;
+    }
+
+    public void saveByHibernateSession(Object object) {
+        try {
+            Session session = entityManager.unwrap(Session.class);
+            Transaction transaction = session.beginTransaction();
+            session.save(object);
+            transaction.commit();
+            session.close();
+        } catch (PersistenceException exception) {
+            System.out.println("Could not unwrap session: " + exception);
+        }
+    }
+
+    public void save (Object object) {
+        entityManager.getTransaction().begin();
+        if (!entityManager.contains(object)) {
+            entityManager.persist(object);
+            entityManager.flush();
+        }
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete (Class clazz, Long id){
+        entityManager.getTransaction().begin();
+        Object toRemove = entityManager.find(clazz, id);
+        entityManager.remove(toRemove);
+        entityManager.getTransaction().commit();
+    }
+
+    public EntityManager getEntityManager(){
+        return entityManager;
+    }
+
+}
